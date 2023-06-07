3533bd6e6264379e14a5f186ec190b21d1f2c67a
Nikita Dolganov <nikitadolganov@MacBook-Air-Nikita.local>
2023-06-07 17:25:49 +0300

    project restructure and model api added

--- services/industry-api/api/main.py
+++ services/industry-api/api/main.py
@@ -1,1 +1,10 @@
-from fastapi import FastAPIfrom finance import finance_router#from settings import settings#app = FastAPI(title="Industry Api", openapi_url=settings.openapi_url)app = FastAPI(title="Industry Api")app.include_router(prefix="/api/finance", tags=['Endpoints'], router=finance_router)
+from fastapi import FastAPI
+from finance import finance_router
+from model import model_router
+#from settings import settings
+
+#app = FastAPI(title="Industry Api", openapi_url=settings.openapi_url)
+app = FastAPI(title="Industry Api")
+
+app.include_router(prefix="/api/finance", tags=['Endpoints'], router=finance_router)
+app.include_router(prefix="/api/model", tags=['Endpoints'], router=model_router)
--- services/industry-api/api/settings.py
+++ services/industry-api/api/settings.py
@@ -1,1 +1,15 @@
-from typing import Optionalfrom pydantic import BaseSettingsclass Settings(BaseSettings):    openapi_url: Optional[str] = None    username: str    password: str    database: strsettings = Settings()
+from typing import Optional
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    openapi_url: Optional[str] = None
+    username: str
+    password: str
+    host: str
+    database: str
+
+
+settings = Settings()
+
